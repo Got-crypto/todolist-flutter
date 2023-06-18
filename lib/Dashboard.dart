35f088f9d20ed09a9f3c72ae657d492a974e388d
Got-crypto <pecpalmplay@gmail.com>
2023-06-18 12:37:25 +0200

    finished the app, my first flutter project

--- lib/Dashboard.dart
+++ lib/Dashboard.dart
@@ -7,6 +7,31 @@ class Dashboard extends StatefulWidget {
 }
 
 class _Dashboard extends State<Dashboard> {
+  final TextEditingController _controller = TextEditingController();
+  List<String> todos = [];
+
+  void _addTodo() {
+    String todo = _controller.text;
+    if (todo.isNotEmpty) {
+      setState(() {
+        todos.add(todo);
+      });
+    }
+    _controller.clear();
+  }
+
+  void _deleteTodo(int index) {
+    setState(() {
+      todos.removeAt(index);
+    });
+  }
+
+  @override
+  void _dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,31 +48,68 @@ class _Dashboard extends State<Dashboard> {
       backgroundColor: Colors.white,
       body: SafeArea(
           child: Container(
-        child: Column(children: [_input(), _input()]),
+        child: Column(children: [_input(), Expanded(child: _todos())]),
       )),
     );
   }
 
   Widget _input() {
     return Container(
-        height: 40,
-        width: 300,
-        margin: const EdgeInsets.only(top: 50, left: 50),
-        child: Row(
-          children: [
-            const Expanded(
-                child: TextField(
-              decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: 'Add your todos here',
-                  contentPadding: EdgeInsets.zero,
-                  fillColor: Colors.amber),
-            )),
-            const SizedBox(
-              width: 10,
+        height: 100,
+        decoration: const BoxDecoration(
+            border: Border(bottom: BorderSide(color: Colors.black12))),
+        width: 500,
+        child: FractionallySizedBox(
+          widthFactor: .8,
+          child: Align(
+            alignment: Alignment.center,
+            child: Row(
+              children: [
+                Expanded(
+                    child: TextField(
+                  controller: _controller,
+                  decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: 'Add your todos here',
+                      contentPadding: EdgeInsets.zero,
+                      fillColor: Colors.amber),
+                )),
+                const SizedBox(
+                  width: 10,
+                ),
+                ElevatedButton(onPressed: _addTodo, child: const Text('Add'))
+              ],
             ),
-            ElevatedButton(onPressed: () {}, child: const Text('Add'))
-          ],
+          ),
         ));
+  }
+
+  Widget _todos() {
+    return Container(
+        width: 500,
+        margin: const EdgeInsets.only(top: 10),
+        child: Align(
+            alignment: Alignment.center,
+            child: FractionallySizedBox(
+              widthFactor: .8,
+              child: ListView.builder(
+                itemCount: todos.length,
+                itemBuilder: (context, index) {
+                  final reversedIndex = todos.length - 1 - index;
+
+                  return ListTile(
+                    title: Text("${index + 1}. ${todos[reversedIndex]}"),
+                    trailing: IconButton(
+                        onPressed: () {
+                          _deleteTodo(reversedIndex);
+                        },
+                        icon: const Icon(
+                          Icons.delete,
+                          color: Colors.red,
+                        )),
+                  );
+                },
+              ),
+            )));
   }
 }
